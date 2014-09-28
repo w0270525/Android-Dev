4227276922b13c810b3de9f59530df2df54cb00d
Jonathan Prince <w0270525@nscc.ca>
2014-09-28 16:04:31 -0300

    Again, trying to add a calculator project to git

---
+++ Jon'sCalculator/app/src/androidTest/java/jonathanprince/nscc/com/jonscalculator/ApplicationTest.java
@@ -0,0 +1,13 @@
+package jonathanprince.nscc.com.jonscalculator;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
