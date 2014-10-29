9bc552d98e7d7488ea4f45e87d522e3634ff8263
jonathan <w0270525@nscc.ca>
2014-10-29 01:55:42 -0300

    finally not shouting at me for not knowing wher the file was

--- Assignment2Quiz/app/src/main/java/com/nscc/jonathanprince/assignment2quiz/QuizActivity.java
+++ Assignment2Quiz/app/src/main/java/com/nscc/jonathanprince/assignment2quiz/QuizActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.widget.*;
 
 //brought in from FileIO demo
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -24,62 +25,66 @@ import java.io.BufferedReader;
 //brought in from hashmap
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Iterator;
+import java.util.Random;
+import java.util.Scanner;
 
 //class for main quiz portion of the app.
 public class QuizActivity extends Activity {
 
-    private static final int READ_BLOCK_SIZE = 100;
+
 
     int questionNumbers=10;//number of questions to create/allow.
     int score=0;//a variable to keep track of scoring
     int questionCounter=0; //variable to keep track of what question the user is on.
 
+    TextView txtCurrentQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        ArrayList<String> answer = new ArrayList<String>();
+        ArrayList<String> answer = new ArrayList<String>();//declare my question and answer arrays.
         ArrayList<String> question = new ArrayList<String>();
+        Map<String,String> map = new HashMap<String,String>();
 
         //check for name value and send by intent to QuizActivity
 
-        String defaultName="";
+        String defaultName="";//default name, should never come in like this.
         Bundle extras= getIntent().getExtras();
         if(extras!=null)
         {
             defaultName = extras.getString("NAME");//name entered on first screen.
 
             try {
-                FileInputStream fIn = openFileInput("quiz.txt");
-                InputStreamReader isr = new InputStreamReader(fIn);
+                //File f = new File( getFilesDir(), "raw/quiz.txt");
+               Scanner scanner;
+                scanner = new Scanner(getAssets().open(("quiz")));
+                        scanner.useDelimiter(";");
+                int count=0;
+                while (scanner.nextLine()!=null){
 
-                char[] inputBuffer = new char[READ_BLOCK_SIZE];
+                    count++;
+                    answer.add(scanner.next());
+                    question.add(scanner.next());
+                    map.put(answer.get(count),question.get(count));
 
-
-                int charRead;
-                while ((charRead = isr.read(inputBuffer))>0)
-                {
-                    //---convert the chars to a String---
-                s += readString;
-
-                    inputBuffer = new char[READ_BLOCK_SIZE];
                 }
-
-
-                answer.add(ar[0]);
-                question.add(ar[1]);
+            scanner.close();
 
 
             }catch(IOException ioe){
                 ioe.printStackTrace();
             }
 
-
+            //randomizes the question order after it's been created and mapped.
+            long seed = System.nanoTime();
+            Collections.shuffle(answer, new Random(seed));
+            Collections.shuffle(question, new Random(seed));
 
 
 /*  TODO hashmap example. remove before finalizing.
@@ -127,4 +132,11 @@ public class QuizActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    //does all the work of setting up the next question for the screen
+    private void nextQuestion(){
+
+        txtCurrentQuestion
+    }
+
 }
